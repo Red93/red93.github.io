cd86a43377e91415f182258e8e9cae9aebf33692
Red93 <thes2life@gmail.com>
2014-05-08 15:10:16 -0400

    php test

---
+++ blog.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Blog</title>
+
+<!--favicon-->
+<link rel="shortcut icon" type="image/png" href="images/favicon.ico"/>
+
+<link rel="stylesheet" type="text/css" href="style.css"/>
+
+<!--Google Web fonts-->
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Wire+One|Chivo">
+<link href='http://fonts.googleapis.com/css?family=Alegreya+Sans+SC:100' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'>
+</head>
+
+<!--body-->
+<body>
+<div class="wrapper">
+
+<!--top page header-->
+<div class="banner">
+<h1 class="header-text text-36px">Leon Williams</h1>
+</div>
+<div class="nav-wrapper">
+<?PHP include 'site-nav.php';?>
+</div>
+<!--page content-->
+<div class="page">
+	<div class="sidebar">
+        <ul class="sidebar">
+            <li><a class="rss" href="rss/rss-blog.xml">
+            Blog
+            </a></li>
+            <li><a class="rss" href="rss/rss-updates.xml">
+            Updates
+            </a></li>
+        <ul>
+    </div>
+    <div class="post">
+    		<span id="rssadd"></span>
+			<h3 class="text-24px title-float">RSS Feed Additions and General Overview</h3>
+			<div class="time-stamp">April 2, 2014</div>
+		<div class="post-content">
+		  <p>Yesterday I added a RSS feed to the site—well, two of them actually: one for blog posts and the other for general updates. For now, they both can be found on this page on the left side. I decided to add these first, that way when I do other things to the site, you'll have these RSS feeds to go by, if you'd like. I put updates and blog posts on two different RSS feeds because... well, you may only be interested in one or the other. The blog feed will, of course, be for blog posts (such as this one) while the updates one will be for generally smaller things I do around the site like adding these RSS feeds, or uploading new Stylish themes to Userstyles.org (my &quot;styles&quot; will be listed on the Projects page once I get that up and running).</p>
+		  <p>Although RSS is very popular, there's still a very large number of people that seem to unaware of how it works or what it is. If you're unfamiliar with RSS, you may already be familiar with the icon or the term even, just by seeing them around often on websites. Simply put, RSS feeds inform you of the latest content on multiple websites, and all you need is a RSS feed reader to use them. If you're the type of person that would like for website updates to come to you as opposed to you having to search for them, then chances are, RSS feeds are probably exactly what you're looking for. Just find an RSS feed reader and input some feed links. They're very easy to use and it's a great alternative to visiting all your bookmarked websites in hopes that you might come across some new content on one of them. Admittedly, I've only known how RSS feeds work for about 2 weeks now, so it's definitely not something that's difficult to get used to.</p>
+		  <p>Also, whether or not you're new to utilizing RSS feeds, you may or may not know that you can add YouTube channels to your list of feeds by inserting the following link with the appropriate YouTube Channel name:</p>
+		  <p>http://gdata.youtube.com/feeds/base/users/<strong>Username</strong>/uploads</p>
+		  <p>It's a lot like subscribing to the user on YouTube except you'll get upload notifications among you other RSS feeds instead of your YouTube subscriptions page. If you have a lot of subscriptions though, then this may not be something you'd care to do, but, it's an option nonetheless (or who knows, perhaps you'd just prefer subscribing on YouTube anyway).</p>
+		  <p>So... &quot;what's next?&quot; you may be wondering. Well, with the RSS feeds on the site and working, I intend to create the About page next because I suppose that it is important if you're on this site in the first place. So, hopefully that'll be up in the next few days to a week, then I plan to get the Projects page up. So we'll see how that goes.</p>
+		</div>
+</div><!--END post-->
+	<div class="post">
+    		<span id="sitelaunch"></span>
+			<h3 class="text-24px title-float">Site Launch!</h3>
+			<div class="time-stamp">March 25, 2014</div>
+		<div class="post-content">
+			<p>Hey everybody! Welcome to the first  website I have built by writing the code for it. This site isn't complete yet, nevertheless, thank you for visiting. If you're reading this on March 25, 2014, then you are among the very first visitors to this site. I plan to add a few more things and features over the next few days, including the &quot;About&quot; and &quot;Projects&quot; page up and running.</p>
+            <p>I have several purposes for this site. I hope to use this website as a general hub for things I do on the web. I used to design texture hacks for Super Smash Bros. Brawl over at Brawl Vault, and I have recently began to write CSS Styles over at Userstyles.org. Anyway, I want to use this site to connect that type of stuff at one central location. As a matter of fact, this site itself is partly practice for me and a place for me to sharpen my web development abilities. I plan for this to be an ongoing project, so you can probably expect to see some changes around here soon enough.</p>
+            <p>Huge thanks to my friend ☆BigGreen for directing me to GitHub and helping me get the whole thing working so I could upload this site. Probably would've taken me a while otherwise.</p>
+            <p>-Leon</p>
+		</div>
+	</div><!--END post-->
+</div>
+</div>
+<!--footer-->
+<div class="banner text-14px footer"><span style="font-family:'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', 'DejaVu Sans', Verdana, sans-serif">&copy;</span> 2014, Leon Williams</div>
+
+</body>
+</html>
